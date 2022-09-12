7a2b24231355013c0c16da9d382119a92b2f860d
DoudiNCer <doudi@outlook.lv>
2022-09-12 09:25:48 +0800

    Init By IntelliJ IDEA

---
+++ src/main/java/com/example/xxsc/XxscApplication.java
@@ -0,0 +1,13 @@
+package com.example.xxsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxscApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xxsc/XxscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xxsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
